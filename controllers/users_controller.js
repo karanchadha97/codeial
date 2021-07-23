e35325e8f3337a5e97ebd8ba475b6147e74e125b
user <karanchadha97>
2021-07-23 10:45:36 +0530

    Render: views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,6 @@
 module.exports.profile = function(req,res){
-    return res.end('<h1>User Profile</h1>');
+  //  return res.end('<h1>User Profile</h1>');
+  return res.render('userProfile',{'title': 'Profile'});
 }
 module.exports.create = function(req,res){
     return res.end('<h1>User Created</h1>');
